dad5fc6d114a0c2debee89f881707174a3bff3c5
kawsar khan <kawsarkhan@gmail.com>
2023-02-15 15:48:45 +0600

    error hendler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +52,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        if($request->expectsJson()){
+
+            if($exception instanceof ModelNotFoundException){
+                return response()->json([
+                    'error' => "Product Model Not Found",
+                ],404);
+            }
+
+            if($exception instanceof NotFoundHttpException){
+                return response()->json([
+                    'error' => "Incorrect Route",
+                ],404); 
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -108,6 +108,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+       $product->delete();
+       return response(Null,204);
+
     }
 }
